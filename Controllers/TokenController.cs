0f970eb6788e26f5183fe90baf998bef691a2450
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-20 04:19:22 +0100

    Adding & configuring the TokenController

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,74 @@
+﻿using InventoryWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        public readonly ApplicationDbContext _context;
+        public TokenController(IConfiguration configuration, ApplicationDbContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserInfo userInfo)
+        {
+            if(userInfo != null && userInfo.UserName != null && userInfo.Password != null)
+            {
+                var user = await GetUser(userInfo.UserName, userInfo.Password);
+                if(user != null)
+                {
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("Id", user.UserId.ToString()),
+                        new Claim("Password", user.Password)
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signin = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.Now.AddMinutes(20),
+                        signingCredentials: signin
+                        );
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid Credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        public async Task<UserInfo> GetUser(string userName, string password)
+        {
+            return await _context.UserInfo.FirstOrDefaultAsync(u => u.UserName == userName && u.Password == password);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -40,6 +40,28 @@ namespace InventoryWebAPI
             services.AddSwaggerGen( c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Web API", Version = "v1"});
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme 
+                {
+                    Description = "JWT Authorization",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
