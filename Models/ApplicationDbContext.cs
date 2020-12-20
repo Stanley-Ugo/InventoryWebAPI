25d0bc26e0312a387783aa74306bbed278fa545b
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-20 02:08:55 +0100

    Adding a connectionstring & registering at startup.cs class

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InventoryWebAPI.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        { }
 
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using InventoryWebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace InventoryWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
