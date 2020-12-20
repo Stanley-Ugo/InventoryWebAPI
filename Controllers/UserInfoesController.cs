e858f8838edfa841492d68bc2b772a53661ffd78
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-20 02:55:38 +0100

    Scaffolding controllers for Products & UserInfo

---
+++ Controllers/UserInfoesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InventoryWebAPI.Models;
+
+namespace InventoryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfoesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserInfoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserInfoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserInfo>>> GetUserInfo()
+        {
+            return await _context.UserInfo.ToListAsync();
+        }
+
+        // GET: api/UserInfoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserInfo>> GetUserInfo(int id)
+        {
+            var userInfo = await _context.UserInfo.FindAsync(id);
+
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return userInfo;
+        }
+
+        // PUT: api/UserInfoes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserInfo(int id, UserInfo userInfo)
+        {
+            if (id != userInfo.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userInfo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserInfoes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<UserInfo>> PostUserInfo(UserInfo userInfo)
+        {
+            _context.UserInfo.Add(userInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserInfo", new { id = userInfo.UserId }, userInfo);
+        }
+
+        // DELETE: api/UserInfoes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserInfo>> DeleteUserInfo(int id)
+        {
+            var userInfo = await _context.UserInfo.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserInfo.Remove(userInfo);
+            await _context.SaveChangesAsync();
+
+            return userInfo;
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return _context.UserInfo.Any(e => e.UserId == id);
+        }
+    }
+}
