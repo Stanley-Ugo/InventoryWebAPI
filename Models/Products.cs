25cb75199ac9eb99ce24cb836dfc90ab9ed91220
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-20 01:46:33 +0100

    Adding the ApplicationDbContext class

--- Models/Products.cs
+++ Models/Products.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,8 @@ namespace InventoryWebAPI.Models
 {
     public class Products
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProdId { get; set; }
         public string ProdName { get; set; }
         public string Category { get; set; }
