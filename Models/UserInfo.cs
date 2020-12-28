eb7e689b8799adf32c5261b9bd2463b977945f87
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-28 05:11:56 +0100

    Making UserId a Required field

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -12,7 +12,7 @@ namespace InventoryWebAPI.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
-        public int UserId { get; set; }
+        public int UserId { get; set; } 
         public string UserName { get; set; }
         public string Password { get; set; }
     }
