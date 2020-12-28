b2449910a1c81c786aa26bcd955fa0c1ad6dca86
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-28 05:11:12 +0100

    Data Annotations

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -11,6 +11,7 @@ namespace InventoryWebAPI.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
